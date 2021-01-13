9564710ecdb03e7217b530293eb5c49cb9efba3f
Florin Lazar <florin.lazar.it2014@gmail.com>
2021-01-13 20:43:37 +0200

    Ctor access level - case 1: protect object instantiation

--- InstanceCtors/InstanceCtors/Program.cs
+++ InstanceCtors/InstanceCtors/Program.cs
@@ -6,7 +6,11 @@ namespace InstanceCtors
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Rectangle r1 = new Rectangle(new Point(10, 10), 100, 50);
+            Rectangle r2 = new Rectangle(new Point(10, 10), new Point(110, 60));
+
+            r1.Print(nameof(r1));
+            r2.Print(nameof(r2));
         }
     }
 }
---
+++ InstanceCtors/InstanceCtors/Rectangle.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace InstanceCtors
+{
+    public class Rectangle
+    {
+        public Rectangle(Point topLeft, int width, int height)
+            : this (topLeft, new Point(topLeft.X + width, topLeft.Y + height), width, height)
+        {
+        }
+
+        public Rectangle(Point topLeft, Point bottomRight)
+            : this (topLeft, bottomRight, Math.Abs(bottomRight.X - topLeft.X), Math.Abs(bottomRight.Y - topLeft.Y))
+        {
+        }
+
+        private Rectangle(Point topLeft, Point bottomRight, int width, int height)
+        {
+            TopLeft = topLeft;
+            BottomRight = bottomRight;
+            Width = width;
+            Height = height;
+        }
+
+        public Point TopLeft { get; }
+
+        public Point BottomRight { get; }
+
+        public int Width { get; }
+
+        public int Height { get; }
+
+        public void Print(string label)
+        {
+            Console.WriteLine($"{label}: Top-Left-X={this.TopLeft.X}, Top-Left-Y={this.TopLeft.Y}");
+            Console.WriteLine($"{label}: Bottom-Right-X={this.BottomRight.X}, Bottom-Right-Y={this.BottomRight.Y}");
+            Console.WriteLine($"{label}: Width={this.Width}, Height={this.Height}");
+        }
+    }
+}
